99f1462066944dd69bd48686a8be72e155d5aacc
pavelpower <pavel.power@gmail.com>
2014-11-15 20:01:28 +0400

    added bind navigation on document.body

--- bem/common.blocks/navigation/navigation.js
+++ bem/common.blocks/navigation/navigation.js
@@ -491,5 +491,19 @@ modules.define('navigation', ['lodash', 'jquery', 'core', 'platform'], function 
         }
     };
 
+    // bind events
+    $(function () {
+        // Navigation events handler
+        $(document.body).bind('nav_key:left nav_key:right nav_key:up nav_key:down', function ( e ) {
+            var cur = Nav.current(),
+                $navs,
+                n;
+
+            $navs = Nav.$container.find(Nav.area_selector).filter(':visible');
+            n = Nav.findNav(cur, e.keyName, $navs);
+            n && Nav.current(n);
+        });
+    });
+
     provide(Nav);
 });
