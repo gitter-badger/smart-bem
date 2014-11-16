be192bd3db4af04bf603d9abb7b8bbc9d569e3d2
Alexej Yaroshevich <zxqfox@gmail.com>
2014-11-16 12:11:33 +0300

    items.bemhtml to bh

--- bem/desktop.blocks/items/items.bh.js
+++ bem/desktop.blocks/items/items.bh.js
@@ -1,5 +1,22 @@
 module.exports = function (bh) {
-    bh.match('items', function(ctx) {
+    bh.match('items', function (ctx) {
         ctx.tag('ul');
     });
+
+	bh.match('items__item', function (ctx) {
+		ctx.tag('li');
+	});
+
+	bh.match('items__title', function (ctx) {
+		ctx.tag('h3');
+	});
+
+	bh.match('items__image', function (ctx) {
+		ctx.tag('img');
+		ctx.attr('src', this.ctx.url);
+	});
+
+	bh.match('items__price', function (ctx) {
+		ctx.tag('span');
+	});
 };
